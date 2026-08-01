a94cf2ac94e910df00ac1f22e60dcd62c8264488
Muhammed Omera <muhammedomera31@gmail.com>
2026-08-02 01:37:15 +0300

    Update BIT_MATH.h

--- Drivers/4_LIB/BIT_MATH.h
+++ Drivers/4_LIB/BIT_MATH.h
@@ -15,10 +15,6 @@
 #define TOG_BIT(reg,bit)		  reg^=(1<<bit)
 #define GET_BIT(reg,bit)          (reg&(1<<bit))>>bit
 
-#define IS_BIT_SET(reg,bit) 	  (reg&(1<<bit))>>bit
-#define IS_BIT_CLR(reg,bit)        !((reg&(1<<bit))>>bit)
-#define ROR(reg,num)               reg=(reg<<(REGISTER_SIZE-num)|(reg>>(num))
-#define ROL(reg,num)               reg=(reg>>(REGISTER_SIZE-num)|(reg<<(num)) 
 
 
 #endif /*BIT_MATH_H_ */
