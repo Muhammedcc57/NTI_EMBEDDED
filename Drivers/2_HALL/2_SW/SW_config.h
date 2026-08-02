0098b53c7015e231546b0b505b253963ce189ead
Muhammed Omera <muhammedomera31@gmail.com>
2026-08-02 08:00:01 +0300

    Add HAL drivers

---
+++ Drivers/2_HALL/2_SW/SW_config.h
@@ -0,0 +1,14 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< SW_config.h >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*   
+*    Author : Mohammed Omera
+*	 Layer : HAL	
+*
+*
+*/
+#ifndef SW_CONFIG_H
+#define SW_CONFIG_H
+
+
+
+#endif
---
+++ Drivers/2_HALL/2_SW/SW_private.h
@@ -0,0 +1,15 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< SW_private.h >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*   
+*    Author : Mohammed Omera
+*	 Layer : HAL	
+*
+*
+*/
+
+#ifndef SW_PRIVATE_H
+#define SW_PRIVATE_H
+
+
+
+#endif
