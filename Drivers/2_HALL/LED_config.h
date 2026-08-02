c8c77701f940a946bc1761868502d97476945c58
Muhammed Omera <muhammedomera31@gmail.com>
2026-08-02 07:47:02 +0300

    Add HAL drivers

---
+++ Drivers/2_HALL/LED_config.h
@@ -0,0 +1,13 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< LED_config.h >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*   
+*    Author : Mohammed Omera
+*	 Layer : HALL	
+*
+*
+*/ 
+#ifndef   LED_CONFIG_H_
+#define   LED_CONFIG_H_
+
+
+#endif
---
+++ Drivers/2_HALL/LED_private.h
@@ -0,0 +1,12 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< LED_private.h >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*   
+*    Author : Mohammed Omera
+*	 Layer : HAL	
+*
+*
+*/ 
+#ifndef LED_PRIVATE_H_
+#define LED_PRIVATE_H_
+
+#endif
