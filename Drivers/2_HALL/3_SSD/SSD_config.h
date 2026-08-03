93e9f496ef2794ab8c1185c23a16a7063f3d8ca5
Muhammed Omera <muhammedomera31@gmail.com>
2026-08-03 18:44:16 +0300

    update drivers

---
+++ Drivers/2_HALL/3_SSD/SSD_config.h
@@ -0,0 +1,13 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< SSD_config.h >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*   
+*    Author : Mohammed Omera
+*	 Layer : HAL	
+*
+*
+*/
+
+#ifndef SSD_CONFIG_H_
+#define SSD_CONFIG_H_
+
+#endif
