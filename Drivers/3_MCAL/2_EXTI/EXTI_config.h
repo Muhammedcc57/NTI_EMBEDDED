37716bf174833e07d48a8f02b81b27ef1e925882
Muhammed Omera <muhammedomera31@gmail.com>
2026-08-03 18:23:42 +0300

    update drivers

---
+++ Drivers/3_MCAL/2_EXTI/EXTI_config.h
@@ -0,0 +1,23 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< EXTI_config.h >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*   
+*    Author : Mohammed Omera
+*	 Layer : MCAL	
+*
+*
+*/
+
+
+#ifndef EXTI_CONFIG_H_
+#define EXTI_CONFIG_H_
+
+
+
+#define EXTI_LINE   EXTI_LINE0
+
+
+
+#define EXTI_SENSE_MODE  EXTI_FALLING_EDGE
+
+
+#endif /* EXTI_CONFIG_H_ */
