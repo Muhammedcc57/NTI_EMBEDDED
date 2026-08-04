4bfdcf2f75bc0e422fa94ecb1f9012fca35ce97b
Muhammed Omera <muhammedomera31@gmail.com>
2026-08-05 01:32:40 +0300

    update

---
+++ Drivers/2_HALL/5_KPD/KPD_interface.h
@@ -0,0 +1,19 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< KPD_interface.h >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*   
+*    Author : Mohammed Omera
+*	 Layer : HAL	
+*
+*
+*/
+
+
+#ifndef KPD_INTERFACE_H_
+#define KPD_INTERFACE_H_
+
+#define NOTPRESSED 0xff
+
+void KPD_Init(void);
+u8   KPD_u8GetPressed( void );
+
+#endif /* KPD_INTERFACE_H_ */
---
+++ Drivers/2_HALL/5_KPD/KPD_private.h
@@ -0,0 +1,16 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< KPD_private.h >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*   
+*    Author : Mohammed Omera
+*	 Layer : HAL	
+*
+*
+*/
+
+ 
+#ifndef KPD_PRIVATE_H_
+#define KPD_PRIVATE_H_
+
+
+
+#endif /* KPD_PRIVATE_H_ */
